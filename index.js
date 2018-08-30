8abbbbc03f32d356846179df8348e5b27ef3eb64
joao <jvsadesign@gmail.com>
2018-08-30 12:00:49 -0300

    urru formulario validado

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const estacoes = require('./data/estacoes.json');
+const expressMongoDb = require('express-mongo-db');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 app.use(bodyParser.urlencoded());
+app.use(expressMongoDb('mongodb://localhost/ranksubway'));
+
 
 app.get('', (req, res) => {
     res.render('index');
@@ -16,23 +19,34 @@ app.get('/estacoes', (req, res) => {
     res.render('estacoes', {'estacoes': estacoes});
 });
 
-app.get('/comentarios', (req, res) => {
-    res.render('comentarios');
+app.post('', (req, res) => {
+
+    let string = `nome ${req.body.nome} \r\nProfissao: ${req.body.profissao} \r\nEmail: ${req.body.email} \r\nMensagem: ${req.body.mensagem}`;
+
+    req.db.collection('mensagem').insert(req.body, (erro) => {
+        console.log(erro);
+        res.render('index');
+    });
+
 });
 
-app.get('/styles.css', (req, res) => {
-    let cores = ['blue', 'red', 'yellow'];
+// app.get('/comentarios', (req, res) => {
+//     res.render('comentarios');
+// });
 
-    let numero = Math.floor(Math.random() * 3);
+// app.get('/styles.css', (req, res) => {
+//     let cores = ['blue', 'red', 'yellow'];
 
-    let cor = cores[numero];
+//     let numero = Math.floor(Math.random() * 3);
+
+//     let cor = cores[numero];
     
-    res.send(`
-        body{
-            color: ${cor};
-        }
-    `);
-});
+//     res.send(`
+//         body{
+//             color: ${cor};
+//         }
+//     `);
+// });
 
 app.listen(3000, () => {
     console.log('Servidor inicializado')
