6b86bb035e40dd63ca919cdaad99bbb94d005389
joao <jvsadesign@gmail.com>
2018-08-31 12:00:43 -0300

    finalizacao

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const estacoes = require('./data/estacoes.json');
 const expressMongoDb = require('express-mongo-db');
-
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
@@ -19,34 +17,38 @@ app.get('/estacoes', (req, res) => {
     res.render('estacoes', {'estacoes': estacoes});
 });
 
+
 app.post('', (req, res) => {
-
+    
     let string = `nome ${req.body.nome} \r\nProfissao: ${req.body.profissao} \r\nEmail: ${req.body.email} \r\nMensagem: ${req.body.mensagem}`;
+    
+    req.db.collection('mensagem').insert(req.body, (erro) => {
+        console.log(erro);
+        res.render('index');
+    });
+    
+    
+});
 
+app.get('/comentarios', (req, res) => {
+    req.db.collection('mensagem').find().toArray((erro, dados) => {
+        console.log(erro);
+        res.render('comentarios', {'comentarios': dados});
+    });
+});
+
+app.post('/comentarios', (req, res) => {
     req.db.collection('mensagem').insert(req.body, (erro) => {
         console.log(erro);
         res.render('index');
     });
 
+
 });
 
-// app.get('/comentarios', (req, res) => {
-//     res.render('comentarios');
-// });
 
-// app.get('/styles.css', (req, res) => {
-//     let cores = ['blue', 'red', 'yellow'];
 
-//     let numero = Math.floor(Math.random() * 3);
 
-//     let cor = cores[numero];
-    
-//     res.send(`
-//         body{
-//             color: ${cor};
-//         }
-//     `);
-// });
 
 app.listen(3000, () => {
     console.log('Servidor inicializado')
